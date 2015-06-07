234c5c117c4b1735aa824188e456ebaa6593d9aa
foxdonut <fdaoud@fastmail.com>
2015-06-07 12:58:44 +0000

    fix babel with mocha

--- client/main.js
+++ client/main.js
@@ -1,13 +1,1 @@
-var React = require("react");
-var pubsub = require("./pubsub/pubsub-jquery");
-
-var bookResource = require("./resource/resource-jquery")("/books");
-require("./books/store")(pubsub, bookResource);
-
-var BookManager = require("./books/bookManager/component");
-
-React.render(
-  <BookManager pubsub={pubsub}/>,
-  document.getElementById("app")
-);
-
+// main.js
