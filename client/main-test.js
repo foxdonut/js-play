680a484b1b28e4cf2354138c6112cdd6fbb3f3ab
foxdonut <fdaoud@fastmail.com>
2015-06-08 13:08:11 -0400

    renamed to generator

--- client/main-test.js
+++ client/main-test.js
@@ -4,10 +4,10 @@ var main = require("./main.es6");
 describe("es6", function() {
   describe("generators", function() {
     it("yields and resumes", function() {
-      var iter1 = main.iter1;
-      var it = main.iter1(5);
+      var generator1 = main.generator1;
+      var it = main.generator1(5);
 
-      // iter1 has 2 yields, so we call next() 3 times.
+      // generator1 has 2 yields, so we call next() 3 times.
       // the first call to next() takes no value, else it is ignored.
       expect(it.next()).to.deep.equal({value: 6, done: false});
       expect(it.next(15)).to.deep.equal({value: 10, done: false});
--- client/main.es6
+++ client/main.es6
@@ -1,11 +1,11 @@
 // main.js
-var iter1 = function*(x) {
+var generator1 = function*(x) {
   var y = 2 * (yield (x + 1));
   var z = yield (y / 3);
   return (x + y + z);
 };
 
 module.exports = {
-  iter1: iter1
+  generator1: generator1
 };
 
