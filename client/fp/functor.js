9b420abbfa85236dfc3276a31caf4bd958dd6071
foxdonut <fdaoud@fastmail.com>
2015-07-27 06:26:34 -0400

    Working on functor exercises.

--- client/fp/functor.js
+++ client/fp/functor.js
@@ -100,7 +100,7 @@ var save = function(x) {
   });
 };
 
-var ex8 = _.compose(_.map(save), ex7);
+var ex8 = _.compose(support.either(support.IO.of, save), ex7);
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
 
